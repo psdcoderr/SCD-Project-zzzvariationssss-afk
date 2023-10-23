07690aefcea9a32bf42f943d2ceb021d237c3861
ayesha <munirayesha009@gmail.com>
2023-10-24 01:13:39 +0500

    comitted interface of roots module
    comitted

--- DAL/Roots_Interface.java
+++ DAL/Roots_Interface.java
@@ -4,6 +4,11 @@ import java.util.List;
 
 public interface Roots_Interface {
     void addRoot(String root);
+    boolean checkRoot(String rName);
+    void updateRoot(String root, String newRoot);
+    void deleteRoot(String root);
+    List<String> showAllRoots();
+    //Made the interface of roots 
   
 }
 
